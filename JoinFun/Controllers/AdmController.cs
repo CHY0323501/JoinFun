6e2e33626c96b47c8cc31ad59bc2da073b6ebc8a
CHY0323501 <a0958288010@gmail.com>
2019-10-26 10:35:52 +0800

    ADM_login_view

---
+++ JoinFun/Controllers/AdmController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoinFun.Controllers
+{
+    public class AdmController : Controller
+    {
+        public ActionResult Login() {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
