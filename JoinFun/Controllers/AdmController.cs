e17b769e709856884db727baefb38f8d08f9e7bc
CHY0323501 <a0958288010@gmail.com>
2019-10-28 20:17:31 +0800

    20191028

--- JoinFun/Controllers/AdmController.cs
+++ JoinFun/Controllers/AdmController.cs
@@ -1,14 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JoinFun.Models;
 
 namespace JoinFun.Controllers
 {
     public class AdmController : Controller
     {
+        SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["dbJoinFun"].ConnectionString);
+        dbJoinFun db = new dbJoinFun();
+        
         public ActionResult Login() {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string account,string pass)
+        {
+            //使用Linq查詢取得帳號(加密用)
+            var getAcc = db.Administrator.Where(m => m.admAcc == account).FirstOrDefault();
+            //查詢管理員帳號及密碼
+            string sql = "select * from Administrator where admAcc=@acc and admPass=@pass";
+            SqlCommand cmd = new SqlCommand(sql,Conn);
+            SqlDataReader reader;
+            
+            
+
+            //取得salt字串
+            string salt = getAcc.admSalt;
+            //產生雜湊
+            byte[] PasswordAndSaltBytes = System.Text.Encoding.UTF8.GetBytes(pass + salt);
+            byte[] HashBytes = new System.Security.Cryptography.SHA256Managed().ComputeHash(PasswordAndSaltBytes);
+            string HashString = Convert.ToBase64String(HashBytes);
+            cmd.Parameters.AddWithValue("@acc", account);
+            cmd.Parameters.AddWithValue("@pass", HashString);
+
+            //連線資料庫並傳回查詢結果
+            Conn.Open();
+            reader = cmd.ExecuteReader();
+
+            //比對結果
+            if (reader.Read()) {
+                //保留登入狀態及管理員暱稱
+                Session["admAccount"] = reader["admAcc"].ToString();
+                Session["admNick"] = reader["admNick"].ToString();
+                //中斷資料庫連線
+                Conn.Close();
+                return RedirectToAction("Index");
+            }
+            Conn.Close();
+            ViewBag.admLoginERR = "您輸入的帳號或密碼錯誤";
             return View();
         }
         public ActionResult Index()
--- JoinFun/Models/metadata.cs
+++ JoinFun/Models/metadata.cs
@@ -1,26 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace JoinFun.Models
 {
-    public class Meta_Acc_Pass
+    public class MetaAcc_Pass
     {
-        [Key]
-        [StringLength(10)]
+        [DisplayName("會員帳號")]
+        [Required(ErrorMessage = "請輸入會員帳號")]
+        [StringLength(10, ErrorMessage = "最多輸入10字")]
         public string Account { get; set; }
-
-        [Required]
+        [DisplayName("會員密碼")]
+        [Required(ErrorMessage = "請輸入會員密碼")]
+        [StringLength(15, ErrorMessage = "最多輸入15字")]
         public string Password { get; set; }
-
-        [Required]
-        [StringLength(10)]
+        [DisplayName("會員編號")]
+        [Required(ErrorMessage = "請輸入會員編號")]
         public string memId { get; set; }
-
-        [Required]
         public string Salt { get; set; }
-
+    }
+    public  class MetaActivity_Class
+    {
+        [DisplayName("類別編號")]
+        [Required(ErrorMessage = "請輸入活動類別編號")]
+        public string actClassId { get; set; }
+        [DisplayName("類別名稱")]
+        [Required(ErrorMessage = "請輸入活動類別名稱")]
+        [StringLength(20, ErrorMessage = "最多輸入20字")]
+        public string actClassName { get; set; }
+        [DisplayName("類別說明")]
+        [Required(ErrorMessage = "請輸入活動類別說明")]
+        [StringLength(50, ErrorMessage = "最多輸入50字")]
+        public string actClassDescrip { get; set; }
+        [DisplayName("類別照片")]
+        [Required(ErrorMessage = "請選擇活動類別照片")]
+        public byte[] Photos { get; set; }
+    }
+    public  class MetaActivity_Details
+    {
+        [DisplayName("會員編號")]
+        [Required(ErrorMessage = "請輸入會員編號")]
+        public string memId { get; set; }
+        [DisplayName("揪團編號")]
+        [Required(ErrorMessage = "請輸入揪團編號")]
+        public string actId { get; set; }
+        [DisplayName("審核狀態")]
+        [Required(ErrorMessage = "請選擇審核狀態")]
+        public bool appvStatus { get; set; }
+        [DisplayName("審核日期")]
+        public Nullable<System.DateTime> appvDate { get; set; }
+    }
+    public  class MetaAdministrator
+    {
+        [DisplayName("管理員編號")]
+        [Required(ErrorMessage = "請輸入管理員編號")]
+        public string admId { get; set; }
+        [DisplayName("管理員帳號")]
+        [Required(ErrorMessage = "請輸入管理員帳號")]
+        [StringLength(10, ErrorMessage = "最多輸入10字")]
+        public string admAcc { get; set; }
+        [DisplayName("管理員密碼")]
+        [Required(ErrorMessage = "請輸入管理員密碼")]
+        [StringLength(15, ErrorMessage = "最多輸入15字")]
+        public string admPass { get; set; }
+        [DisplayName("管理員暱稱")]
+        [Required(ErrorMessage = "請輸入管理員暱稱")]
+        [StringLength(10, ErrorMessage = "最多輸入10字")]
+        public string admNick { get; set; }
+        public string admSalt { get; set; }
+    }
+    public  class MetaAge_Restriction
+    {
+        [DisplayName("年齡限制編號")]
+        [Required(ErrorMessage = "請輸入年齡限制編號")]
+        public short serial { get; set; }
+        [DisplayName("年齡限制")]
+        [Required(ErrorMessage = "請輸入年齡限制")]
+        [StringLength(8, ErrorMessage = "最多輸入8字")]
+        public string age { get; set; }
+    }
+    public  class MetaBlacklist
+    {
+        [DisplayName("會員編號")]
+        [Required(ErrorMessage = "請輸入會員編號")]
+        public string blockedMemId { get; set; }
+        [DisplayName("黑名單對象會員編號")]
+        [Required(ErrorMessage = "請輸入黑名單對象會員編號")]
+        public string memId { get; set; }
     }
 }
