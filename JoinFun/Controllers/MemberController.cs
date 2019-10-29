d236241445b7a0d242925e0b4791f9b76acfa5c5
CHY0323501 <53595498+CHY0323501@users.noreply.github.com>
2019-10-29 13:26:22 +0800

    20191029test

--- JoinFun/Controllers/MemberController.cs
+++ JoinFun/Controllers/MemberController.cs
@@ -15,19 +15,20 @@ namespace JoinFun.Controllers
         JoinFunEntities db = new JoinFunEntities();
         public ActionResult Info(string memID="M000000001")
         {
-            MemberViewModel mem = new MemberViewModel()
-            {
-                Member = db.Member.Where(m => m.memId == memID).ToList(),
-                Acc_Pass = db.Acc_Pass.Where(m => m.memId == memID).ToList(),
-                Blacklist = db.Blacklist.Where(m => m.memId == memID).ToList(),
-                Bookmark_Details = db.Bookmark_Details.Where(m => m.memId == memID).ToList(),
-                Dietary_Preference=db.Dietary_Preference.Where(m => m.memId == memID).ToList(),
-                Fans=db.Fans.Where(m => m.memId == memID).ToList(),
-                FollowUp=db.FollowUp.Where(m => m.memId == memID).ToList(),
-                Friendship=db.Friendship.Where(m => m.memId == memID).ToList(),
-                Habit=db.Habit.Where(m => m.memId == memID).ToList(),
-                Member_Remarks=db.Member_Remarks.Where(m => m.FromMemId == memID).ToList()
-            };
+            //MemberViewModel mem = new MemberViewModel()
+            //{
+            //    Member = db.Member.Where(m => m.memId == memID).ToList(),
+            //    Acc_Pass = db.Acc_Pass.Where(m => m.memId == memID).ToList(),
+            //    Blacklist = db.Blacklist.Where(m => m.memId == memID).ToList(),
+            //    Bookmark_Details = db.Bookmark_Details.Where(m => m.memId == memID).ToList(),
+            //    Dietary_Preference=db.Dietary_Preference.Where(m => m.memId == memID).ToList(),
+            //    Fans=db.Fans.Where(m => m.memId == memID).ToList(),
+            //    FollowUp=db.FollowUp.Where(m => m.memId == memID).ToList(),
+            //    Friendship=db.Friendship.Where(m => m.memId == memID).ToList(),
+            //    Habit=db.Habit.Where(m => m.memId == memID).ToList(),
+            //    Member_Remarks=db.Member_Remarks.Where(m => m.FromMemId == memID).ToList()
+            //};
+            var mem = db.Member.Where(m => m.memId == memID).ToList();
             return View(mem);
         }
     }
