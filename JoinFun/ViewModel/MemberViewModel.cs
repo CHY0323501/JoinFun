60ee25fc91e009f57a384365d0117c9d7c41a3f1
芳飴 吳 <bowbow19@gmail.com>
2019-11-07 17:05:58 +0800

    111

--- JoinFun/ViewModel/MemberViewModel.cs
+++ JoinFun/ViewModel/MemberViewModel.cs
@@ -13,7 +13,6 @@ namespace JoinFun.ViewModel
         public List<Blacklist> Blacklist { get; set; }
         public List<Bookmark_Details> Bookmark_Details { get; set; }
         public List<vw_Member_Remarks> vw_Member_Remarks { get; set; }
-        public List<vw_Activities> vw_Activities { get; set; }
         public List<vw_FansNew> vw_FansNew { get; set; }
         public List<vw_FollowUp> vw_FollowUp { get; set; }
         public List<vw_FriendShip> vw_FriendShip { get; set; }
