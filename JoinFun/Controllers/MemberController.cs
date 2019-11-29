d439291c8652a606e0e791a992efcadf9ef0ced8
CHY0323501 <53595498+CHY0323501@users.noreply.github.com>
2019-11-29 20:12:23 +0800

    再次修正好友功能
    真的很奇怪修好了又壞QQ

--- JoinFun/Controllers/MemberController.cs
+++ JoinFun/Controllers/MemberController.cs
@@ -37,7 +37,7 @@ namespace JoinFun.Controllers
             }
             else
             {
-                //Session["memid"] = "M000000008";
+                //Session["memid"] = "M000000002";
                 MemberViewModel Minfo = new MemberViewModel()
                 {
                     Member = db.Member.Where(m => m.memId == memID).ToList(),
